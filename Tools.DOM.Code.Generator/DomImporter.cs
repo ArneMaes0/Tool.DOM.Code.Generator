5007ff7d3ee6afb17dd1586ac97b6eedf8d7796f
Arne Maes <arne.maes@skyline.be>
2024-09-16 16:30:17 +0200

    initialize

---
+++ Tools.DOM.Code.Generator/DomImporter.cs
@@ -0,0 +1,249 @@
+﻿namespace Skyline.DataMiner.Tools.DOM.Code.Generator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Linq;
+    using System.Text;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    using Skyline.DataMiner.Generator.DOM.Builders;
+    using Skyline.DataMiner.Generator.DOM.Contexts;
+    using Skyline.DataMiner.Net.Apps.DataMinerObjectModel;
+    using Skyline.DataMiner.Net.Apps.Modules;
+    using Skyline.DataMiner.Net.LogHelpers;
+    using Skyline.DataMiner.Net.ManagerStore;
+    using Skyline.DataMiner.Net.Messages;
+    using Skyline.DataMiner.Net.Messages.SLDataGateway;
+    using Skyline.DataMiner.Net.Sections;
+
+    public class DomImporter
+    {
+        private static readonly JsonSerializer JsonSerializer = JsonSerializer.Create(new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            ContractResolver = new DefaultContractResolver { IgnoreSerializableInterface = true }
+        });
+
+        private readonly List<ModuleContext> modules = new List<ModuleContext>();
+        private JsonTextReader jsonTextReader;
+
+        public DomImporter()
+        {
+        }
+
+        public List<ModuleContext> Import(string path)
+        {
+            try
+            {
+                using (var reader = GetReader(path))
+                {
+                    jsonTextReader = reader.JsonTextReader;
+                    jsonTextReader.Read(); // start array
+                    while (jsonTextReader.Read() && jsonTextReader.TokenType == JsonToken.StartObject)
+                    {
+                        ImportModule();
+                        jsonTextReader.Read(); // end object
+                    }
+                }
+
+                return modules;
+            }
+            catch (IOException e)
+            {
+                throw new IOException(e.Message, e);
+            }
+            catch (JsonException e)
+            {
+                throw new JsonException("File has an invalid structure.", e);
+            }
+        }
+
+        private List<CustomSectionDefinition> ImportSectionDefinitions()
+        {
+            var sections = new List<CustomSectionDefinition>();
+
+            jsonTextReader.Read();
+            jsonTextReader.Read();
+            while (jsonTextReader.Read() && jsonTextReader.TokenType == JsonToken.StartObject)
+            {
+                var sectionDefinition = JsonSerializer.Deserialize<CustomSectionDefinition>(jsonTextReader);
+                sections.Add(sectionDefinition);
+            }
+
+            return sections;
+        }
+
+        private List<DomBehaviorDefinition> ImportDomBehaviorDefinitions()
+        {
+            var behaviors = new List<DomBehaviorDefinition>();
+
+            jsonTextReader.Read();
+            jsonTextReader.Read();
+            while (jsonTextReader.Read() && jsonTextReader.TokenType == JsonToken.StartObject)
+            {
+                var behaviorDefinition = JsonSerializer.Deserialize<DomBehaviorDefinition>(jsonTextReader);
+                behaviors.Add(behaviorDefinition);
+            }
+
+            return behaviors;
+        }
+
+        private List<DomDefinition> ImportDomDefinitions()
+        {
+            var definitions = new List<DomDefinition>();
+
+            jsonTextReader.Read();
+            jsonTextReader.Read();
+            while (jsonTextReader.Read() && jsonTextReader.TokenType == JsonToken.StartObject)
+            {
+                var domDefinition = JsonSerializer.Deserialize<DomDefinition>(jsonTextReader);
+                definitions.Add(domDefinition);
+            }
+
+            return definitions;
+        }
+
+        private List<DomTemplate> ImportDomTemplates()
+        {
+            var domTemplates = new List<DomTemplate>();
+
+            jsonTextReader.Read();
+            jsonTextReader.Read();
+            while (jsonTextReader.Read() && jsonTextReader.TokenType == JsonToken.StartObject)
+            {
+                var domTemplate = JsonSerializer.Deserialize<DomTemplate>(jsonTextReader);
+                domTemplates.Add(domTemplate);
+            }
+
+            return domTemplates;
+        }
+
+        private List<DomInstance> ImportDomInstances()
+        {
+            var domInstances = new List<DomInstance>();
+
+            jsonTextReader.Read();
+            jsonTextReader.Read();
+            while (jsonTextReader.Read() && jsonTextReader.TokenType == JsonToken.StartObject)
+            {
+                var domInstance = JsonSerializer.Deserialize<DomInstance>(jsonTextReader);
+                domInstances.Add(domInstance);
+            }
+
+            return domInstances;
+        }
+
+        private void ImportModule()
+        {
+            jsonTextReader.Read(); // property name
+            jsonTextReader.Read(); // start object
+            var moduleSettings = JsonSerializer.Deserialize<ModuleSettings>(jsonTextReader);
+
+            var module = BuilderFactory.CreateModuleBuilder()
+                .AddModuleId(moduleSettings.ModuleId)
+                .AddSections(ImportSectionDefinitions())
+                .AddBehaviors(ImportDomBehaviorDefinitions())
+                .AddDefinitions(ImportDomDefinitions());
+
+            _ = ImportDomTemplates();
+            _ = ImportDomInstances();
+
+            modules.Add(module.Build());
+        }
+
+        private IModuleReader GetReader(string path)
+        {
+            if (path.EndsWith(".json"))
+            {
+                return new ModuleReader(path);
+            }
+            else if (path.EndsWith(".zip"))
+            {
+                return new ZipReader(path);
+            }
+            else
+            {
+                throw new NotSupportedException("The given filetype is not supported");
+            }
+        }
+
+        private interface IModuleReader : IDisposable
+        {
+            JsonTextReader JsonTextReader { get; }
+        }
+
+        private sealed class ZipReader : IModuleReader
+        {
+            private readonly FileStream fileStream;
+            private readonly ZipArchive zipArchive;
+            private readonly Stream stream;
+            private readonly StreamReader streamReader;
+
+            public ZipReader(string path)
+            {
+                try
+                {
+                    fileStream = new FileStream(path, FileMode.Open);
+                    zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Read);
+                    stream = zipArchive.Entries.Single().Open();
+                    streamReader = new StreamReader(stream, Encoding.UTF8);
+                    JsonTextReader = new JsonTextReader(streamReader);
+                    JsonTextReader.SupportMultipleContent = true;
+                }
+                catch
+                {
+                    Dispose();
+                    throw;
+                }
+            }
+
+            public JsonTextReader JsonTextReader { get; }
+
+            public void Dispose()
+            {
+                ((IDisposable)JsonTextReader)?.Dispose();
+                streamReader?.Dispose();
+                stream?.Dispose();
+                zipArchive?.Dispose();
+                fileStream?.Dispose();
+            }
+        }
+
+        private sealed class ModuleReader : IModuleReader
+        {
+            private readonly FileStream fileStream;
+            private readonly Stream stream;
+            private readonly StreamReader streamReader;
+
+            public ModuleReader(string path)
+            {
+                try
+                {
+                    fileStream = new FileStream(path, FileMode.Open);
+                    streamReader = new StreamReader(fileStream, Encoding.UTF8);
+                    JsonTextReader = new JsonTextReader(streamReader);
+                    JsonTextReader.SupportMultipleContent = true;
+                }
+                catch
+                {
+                    Dispose();
+                    throw;
+                }
+            }
+
+            public JsonTextReader JsonTextReader { get; }
+
+            public void Dispose()
+            {
+                ((IDisposable)JsonTextReader)?.Dispose();
+                streamReader?.Dispose();
+                stream?.Dispose();
+                fileStream?.Dispose();
+            }
+        }
+    }
+}
---
+++ Tools.DOM.Code.Generator/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Skyline.DataMiner.Tools.DOM.Code.Generator
+{
+    using System;
+    using System.CommandLine;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Logging;
+
+    using Serilog;
+
+    using Skyline.DataMiner.Generator.DOM;
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public static class Program
+    {
+        /*
+         * Design guidelines for command line tools: https://learn.microsoft.com/en-us/dotnet/standard/commandline/syntax#design-guidance
+         */
+
+        /// <summary>
+        /// Code that will be called when running the tool.
+        /// </summary>
+        /// <param name="args">Extra arguments.</param>
+        /// <returns>0 if successful.</returns>
+        public static async Task<int> Main(string[] args)
+        {
+            var isDebug = new Option<bool>(
+            name: "--debug",
+            description: "Indicates the tool should write out debug logging.")
+            {
+                IsRequired = false,
+            };
+
+            isDebug.SetDefaultValue(true);
+
+            var fileArgument = new Option<FileInfo>(
+                name: "--file",
+                description: "The path to the file containing the DOM modules.")
+            {
+                IsRequired = true
+            };
+
+            var rootCommand = new RootCommand("")
+            {
+                isDebug,
+                fileArgument,
+            };
+
+            rootCommand.SetHandler(Process, isDebug, fileArgument);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        private static async Task<int> Process(bool isDebug, FileInfo file)
+        {
+            try
+            {
+                var logConfig = new LoggerConfiguration().WriteTo.Console();
+                logConfig.MinimumLevel.Is(isDebug ? Serilog.Events.LogEventLevel.Debug : Serilog.Events.LogEventLevel.Information);
+                var seriLog = logConfig.CreateLogger();
+
+                using(var loggerFactory = LoggerFactory.Create(builder => builder.AddSerilog(seriLog)))
+                {
+                    var logger = loggerFactory.CreateLogger("Skyline.DataMiner.Tools.DOM.Code.Generator");
+                    try
+                    {
+                        //Main Code for program here
+                        var importer = new DomImporter();
+                        var code = CodeGenerator.Generate(importer.Import(file.FullName));
+                        Console.WriteLine(code);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.LogError($"Exception during Process Run: {e}");
+                        return 1;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception on Logger Creation: {e}");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
